58803e0933ad0a5aefb853961a88579087842542
unknown <liehn.eduard1@gmail.com>
2023-06-05 00:33:21 +0300

    yay

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -3,6 +3,7 @@ import { Home } from "./pages/home";
 import { Auth } from "./pages/auth";
 import { CreateRecipe } from "./pages/create-recipe";
 import { SavedRecipes } from "./pages/saved-recipes";
+import { Navbar } from "./components/Navbar";
 
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
